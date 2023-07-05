477ecfa92d1b815ad27a1f834347f9227babeeca
Elena Imyarekova <elena.imyarekova1@gmail.com>
2023-07-05 15:55:49 +0300

    src/main/java/BonusService.java and src/test/java/BonusServiceTest.java

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -1,11 +1,12 @@
 public class BonusService {
-    public long calculate(long amount, boolean registered) {
-        int percent = registered ? 3 : 1;
+    public long calculate(long amount, boolean Registered) {
+        int percent = Registered ? 3 : 1;
         long bonus = amount * percent / 100;
         long limit = 500;
         if (bonus > limit) {
             bonus = limit;
         }
         return bonus;
+
     }
 }
--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -20,12 +20,44 @@ public class BonusServiceTest {
     }
 
     @Test
+    void shouldCalculateForUnRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
     void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
         long amount = 1_000_000;
         boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateUnRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        int amount = 1_000_000;
+        boolean registered = false;
         long expected = 500;
 
         // вызываем целевой метод:
